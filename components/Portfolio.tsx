61e7250fcc3a93a4e30221519f80f72190ca2972
Osan Rai <osanbantawarai@gmail.com>
2025-07-19 21:18:35 +0545

    Refactor code structure for improved readability and maintainability

---
+++ components/Portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <main>
+      <h1 className='marlet-font text-4xl'>LEOSS</h1>
+      <h1 className='text-6xl'>hello world</h1>
+      <p className='text-6xl'>hello</p>
+    </main>
+  )
+}
+
+export default Portfolio
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                marlet: ['Marlet', 'avenir']
+            }
+        }
+    }
+}
