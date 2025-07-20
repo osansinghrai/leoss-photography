dc37e990f1463147958ca51c99d1b9048328d22e
Osan Rai <osanbantawarai@gmail.com>
2025-07-20 11:54:27 +0545

    feat: implement header navigation with dynamic routing and add search functionality style: add text-shadow to marlet font for improved visibility feat: create placeholder pages for contact, gallery, and portfolio sections

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client";
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+interface navigateProps {
+  label: string;
+  path: string;
 }
 
-export default Header
+const Header = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const [activeButton, setActiveButton] = React.useState("");
+  const [showSearchInput, setShowSearchInput] = React.useState(false);
+
+  const navButtons = [
+    { label: "Home", path: "/" },
+    { label: "Portfolio", path: "/portfolio" },
+    { label: "Experience", path: "/experience" },
+    { label: "Gallery", path: "/gallery" },
+    { label: "Contact", path: "/contact" },
+  ];
+
+  useEffect(() => {
+    const currentNav = navButtons.find((item) => item.path === pathName);
+    if (currentNav) setActiveButton(currentNav.label);
+  }, [pathName]);
+
+  const handleClick = ({ label, path }: navigateProps) => {
+    setActiveButton(label);
+    router.push(path);
+  };
+
+  const handleBlur = () => {
+    setShowSearchInput(false);
+  };
+
+  return (
+    <main className="flex flex-row justify-center items-center ml-16 mt-4 mr-16">
+      <div>
+        <Link href="/">
+          <h1 className="marlet-font text-4xl cursor-pointer text-shadow-font ">
+            LEOSS
+          </h1>
+        </Link>
+      </div>
+      <div className="flex  justify-center items-center gap-10 ml-auto mr-auto">
+        {navButtons.map((item) => (
+          <button
+            onClick={() => handleClick(item)}
+            key={item.label}
+            className={`hover:font-black hover:text-lg transition-all duration-300 ease-in-out cursor-pointer pb-2 border-b-2 ${
+              activeButton === item.label
+                ? "font-black border-black"
+                : "border-transparent"
+            }`}
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
+      <div className=" w-[120px] cursor-pointer transition-all duration-300 ease-in-out">
+        {showSearchInput ? (
+          <input
+            type="text"
+            placeholder="Search"
+            autoFocus
+            width={28}
+            height={28}
+            onBlur={handleBlur}
+            className="border-b-2 border-black outline-none pl-1 pr-2 -translate-x-10"
+          />
+        ) : (
+          <Image
+            src="/search.png"
+            alt="search"
+            width={28}
+            height={28}
+            onClick={() => setShowSearchInput(true)}
+            
+          ></Image>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Header;
